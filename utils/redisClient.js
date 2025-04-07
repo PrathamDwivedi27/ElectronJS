393ed9eeafd876363641a518abb4592ceaa12fcb
Pratham-fursatAI <prathamd549@gmail.com>
2025-04-07 18:19:21 +0530

    [feat]: minor changes

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -3,8 +3,8 @@ const IORedis = require("ioredis");
 const connection = new IORedis({
   host: "localhost",
   port: 6379,
-  maxRetriesPerRequest: null, // ✅ IMPORTANT for BullMQ workers
-  enableReadyCheck: false,    // ✅ Optional: avoids extra delays
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,  
 });
 
 module.exports = connection;
